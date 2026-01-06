2fe3956764023d77c252de25a63a8de6f44cd1d8
Lê Tấn <42458820+letan-it@users.noreply.github.com>
2026-01-06 18:23:14 +0700

    Update app.footer.ts

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
     standalone: true,
     selector: 'app-footer',
     template: `<div class="layout-footer">
-        SAKAI by
-        <a href="https://primeng.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeNG</a>
+        AI Agents by
+        <a href="https://letan-it.github.io/Portfolio/" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Lê Quốc Tấn</a>
     </div>`
 })
 export class AppFooter {}
