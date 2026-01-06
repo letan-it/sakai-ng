c0b5f37e64e9a6e679ca054bc7861ac216d35669
Lê Tấn <42458820+letan-it@users.noreply.github.com>
2026-01-06 18:22:09 +0700

    Update app.topbar.ts

--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -33,7 +33,7 @@ import { LayoutService } from '../service/layout.service';
                         />
                     </g>
                 </svg>
-                <span>SAKAI</span>
+                <span>AI Agents - LQT</span>
             </a>
         </div>
 
